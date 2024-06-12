1e4f95cf64005190aff1938530d5b8b698db7fdf
Logan Ward <ward.logan.t@gmail.com>
2024-06-12 09:45:26 -0400

    Initial model for a single-resistor circuit

---
+++ asoh/models/__init__.py
@@ -0,0 +1,3 @@
+"""Models for energy storage systems which conform to the
+`Prognostic Model <https://nasa.github.io/progpy/api_ref/progpy/PrognosticModel.html#progpy.PrognosticsModel>`_
+interface from ProgPy"""
