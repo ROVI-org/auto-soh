f29fcf59439902441d7b55207632b566ce1e8709
Victor Venturi <vventuri@Victors-MBP.hsd1.il.comcast.net>
2024-06-25 15:50:15 -0500

    More formatting corrections

--- asoh/models/base.py
+++ asoh/models/base.py
@@ -13,10 +13,10 @@ class GeneralContainer(BaseModel):
     @property
     def names(self) -> tuple[str, ...]:
         return tuple(self.model_fields.keys())
-    
+
     @property
     def all_names(self) -> tuple[str, ...]:
-        all_names=self.names
+        all_names = self.names
         if ('extra', 'allow') in self.model_config.items():
             all_names += tuple(self.model_extra.keys())
         return all_names
@@ -29,13 +29,13 @@ class InputQuantities(GeneralContainer,
     Inputs quantities to a battery model, such as time, current, and temperature
     readings.
     """
-    time: float=Field(description='Timestamp(s) of inputs. Units: s')
-    current: float=Field(
+    time: float = Field(description='Timestamp(s) of inputs. Units: s')
+    current: float = Field(
         description='Current(s) applied to the storage system. Units: A')
-    temperature: Optional[float]=\
+    temperature: Optional[float] = \
         Field(default='Not provided!',
               description='Temperature reading(s). Units: °C')
-    
+
     def to_numpy(self,
                  additional_inputs: tuple[str, ...]=()) -> np.ndarray:
         """
@@ -55,18 +55,18 @@ class InputQuantities(GeneralContainer,
         return np.array(combined)
 
 
-class OutputMeasurements(GeneralContainer, 
+class OutputMeasurements(GeneralContainer,
                          arbitrary_types_allowed=True,
                          validate_assignment=True):
     """
     Ouput measurement of a battery model. Must include terminal voltage. 
     """
-    terminal_voltage: float=\
+    terminal_voltage: float = \
         Field(description='Voltage output of a battery cell/model. Units: V')
-    
-    def to_numpy(self, 
+
+    def to_numpy(self,
                  additional_outputs: tuple[str, ...]=()) -> np.ndarray:
-        combined=[getattr(self, field) for field in self.names]
+        combined = [getattr(self, field) for field in self.names]
         combined += [getattr(self, add_out) for add_out in additional_outputs]
         return np.array(combined)
 
@@ -78,7 +78,7 @@ class HiddenVector(GeneralContainer,
     Holds the physical transient hidden state quantities (example: SOC, etc.)
     """    
     def to_numpy(self) -> np.ndarray:
-        transient_state=tuple(getattr(self, hid_var) \
+        transient_state = tuple(getattr(self, hid_var) \
                                 for hid_var in self.names)
         return np.hstack(transient_state)
 
@@ -91,7 +91,7 @@ class HealthParameter(BaseModel,
     """
     @abstractmethod
     def to_numpy(self) -> np.ndarray:
-        pass 
+        pass
 
 
 class AdvancedStateOfHealth(GeneralContainer,
@@ -102,9 +102,9 @@ class AdvancedStateOfHealth(GeneralContainer,
     """
     def to_numpy(self,
                  additional_health: tuple[str, ...]=()) -> np.ndarray:
-        asoh=np.array([])
+        asoh = np.array([])
         for health_param in self.names:
-            asoh=np.hstack((asoh, getattr(self, health_param).to_numpy()))
+            asoh = np.hstack((asoh, getattr(self, health_param).to_numpy()))
         return asoh
 
 
@@ -116,15 +116,15 @@ class JointState(BaseModel,
     That is, it stores the physical transient hidden state (example: SOC, etc.),
     as well as the A-SOH model parameters (example: Q_total, R_0, etc.)
     """
-    transient_state: HiddenVector=\
+    transient_state: HiddenVector = \
         Field(description='Physical transient hidden state')
-    asoh: AdvancedStateOfHealth=\
+    asoh: AdvancedStateOfHealth = \
         Field(description='Advanced State of Health (A-SOH)')
-    
+
     @property
     def joint_names(self) -> tuple[str, ...]:
         return self.transient_state.names + self.asoh.names
-    
+
     def to_numpy(self) -> np.ndarray:
         return np.hstack((self.transient_state.to_numpy(),
                           self.asoh.to_numpy()))
