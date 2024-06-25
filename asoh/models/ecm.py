e26247aff66e8d7ef9c656f011ce4aa6cb858d05
Victor Venturi <vventuri@Victors-MBP.hsd1.il.comcast.net>
2024-06-25 17:45:01 -0500

    additional formatting changes

--- asoh/models/ecm.py
+++ asoh/models/ecm.py
@@ -35,10 +35,10 @@ class MaxTheoreticalCapacity(HealthParameter):
     Defines maximum theoretical discharge capacity of a cell
     """
     Q_t: float = \
-        Field(description=
+        Field(description=\
               'Maximum theoretical discharge capacity of a cell. Units: Amp-hour')
-    updatable: bool = Field(default=True, 
-                            description=
+    updatable: bool = Field(default=True,
+                            description=\
                             'Define if external model can update this variable.')
 
     @property
@@ -52,29 +52,29 @@ class MaxTheoreticalCapacity(HealthParameter):
         if self.updatable:
             return 3600 * np.array(self.Q_t)
         return np.array([])
-    
+
 
 class SeriesResistance(HealthParameter):
     """
     Defines the series resistance component of an ECM.
     """
     internal_parameters: Union[float, List, np.ndarray] = \
-        Field(description=
+        Field(description=\
               'Values of series resistance at specified SOCs. Units: Ohm')
     soc_pinpoints: Optional[Union[List, np.ndarray]] = \
         Field(default=[], description='SOC pinpoints for interpolation.')
-    updatable: bool = Field(default=True, 
-                            description=
+    updatable: bool = Field(default=True,
+                            description=\
                             'Define if external model can update this variable.')
     interpolation_style: \
-        Literal['linear', 'nearest', 'nearest-up', 'zero', 'slinear', \
+        Literal['linear', 'nearest', 'nearest-up', 'zero', 'slinear',
                 'quadratic', 'cubic', 'previous', 'next'] = \
         Field(default='linear', description='Type of interpolation to perform')
     reference_temperature: Optional[float] = \
-        Field(default=25, 
+        Field(default=25,
               description='Reference temperature for internal parameters. Units: °C')
     temperature_dependence_factor: Optional[float] = \
-        Field(default=0, 
+        Field(default=0,
               description='Factor determining dependence of R0 with temperature. Units: 1/°C')
 
     @computed_field
@@ -85,7 +85,7 @@ class SeriesResistance(HealthParameter):
         internal_parameters are evenly spread on an SOC interval [0,1].
         """
         if not len(self.soc_pinpoints):
-            self.soc_pinpoints = np.linspace(0,1,len(self.internal_parameters))
+            self.soc_pinpoints = np.linspace(0, 1, len(self.internal_parameters))
         func = interp1d(self.soc_pinpoints,
                         self.internal_parameters,
                         kind=self.interpolation_style,
@@ -95,7 +95,7 @@ class SeriesResistance(HealthParameter):
 
     def value(self, soc, temp=None) -> Union[float, np.ndarray]:
         """
-        Computes value of series resistance at a given SOC and temperature. 
+        Computes value of series resistance at a given SOC and temperature.
         """
         reference_value = self._interp_func(soc)
         if temp is None or self.temperature_dependence_factor == 0:
@@ -134,8 +134,8 @@ class ECM_ASOH(AdvancedStateOfHealth):
             if f_annotation:
                 new_annotations[f_name] = f_annotation
 
-            new_fields[f_name] = FieldInfo(annotation=f_annotation, 
-                                           default = f_value)
+            new_fields[f_name] = FieldInfo(annotation=f_annotation,
+                                           default=f_value)
 
         cls.model_fields.update(new_fields)
         cls.model_rebuild(force=True)
