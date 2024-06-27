8d9c55321a3d0d32842f2dbc95bbb6227e62d797
victorventuri <victorventuri95@gmail.com>
2024-06-27 18:03:26 -0500

    Starting ECM definitions

--- asoh/models/ecm.py
+++ asoh/models/ecm.py
@@ -1,14 +1,85 @@
+from typing import Any, Dict, Optional
 import numpy as np
 from pydantic import Field
+from pydantic.fields import FieldInfo
 
-from .base import HealthModel, InputState, SystemState, Measurements
+from .base import InputQuantities, OutputMeasurements, HealthParameter
+from .base import AdvancedStateOfHealth
 
-
-class ECMInput(InputState):
-    """Control of a battery based on the feed current"""
-
+################################################################################
+##                              INPUTS & OUTPUT                               ##
+################################################################################
+class ECM_Input(InputQuantities):
+    """
+    Control of a battery based on the feed current, temperature
+    """
     pass
 
+# TODO (vventuri): Remeber we need to implement ways to denoise SOC, Qt, R0, 
+#                   which require more outputs
+class ECM_Measurement(OutputMeasurements):
+    """
+    Controls the outputs of the ECM.
+    """
+    pass
+
+################################################################################
+##                               HEALTH METRICS                               ##
+################################################################################
+class MaxTheoreticalCapacity(HealthParameter):
+    """
+    Defines maximum theoretical discharge capacity of a cell
+    """
+    Q_t: float = \
+        Field(description = 'Maximum theoretical discharge capacity of a cell. Units: Amp-hour')
+    updatable: bool = Field(default = True, description = '')
+    
+    def to_numpy(self) -> np.array:
+        """
+        Returns maximum theoretical discharge capacity in Amp-seconds
+        """
+        return 3600 * np.array(self.Q_t)
+    
+
+class SeriesResistance(HealthParameter):
+    """
+    Defines the series resistance component of an ECM.
+    """
+    pass
+
+
+
+class ECM_ASOH(AdvancedStateOfHealth):
+    pass
+
+    def add_fields(cls, **field_definitions: Any):
+        new_fields: Dict[str, FieldInfo] = {}
+        new_annotations: Dict[str, Optional[type]] = {}
+
+        for f_name, f_def in field_definitions.items():
+            if isinstance(f_def, tuple):
+                try:
+                    f_annotation, f_value = f_def
+                except ValueError as e:
+                    raise Exception(
+                        'field definitions should either be a tuple of (<type>, <default>) or just a '
+                        'default value, unfortunately this means tuples as '
+                        'default values are not allowed'
+                    ) from e
+            else:
+                f_annotation, f_value = None, f_def
+
+            if f_annotation:
+                new_annotations[f_name] = f_annotation
+
+            new_fields[f_name] = FieldInfo(annotation=f_annotation, 
+                                           default = f_value)
+
+        cls.model_fields.update(new_fields)
+        cls.model_rebuild(force=True)
+
+
+################################################################################
 
 class ECMState(SystemState):
     """State of a battery defined by an Equivalent circuit model"""
