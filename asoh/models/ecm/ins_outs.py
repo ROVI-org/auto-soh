069e265649c76cca9a988168f8516b3d94ce5f57
victorventuri <victorventuri95@gmail.com>
2024-07-03 21:17:00 -0500

    Defining ECM output container
    Added a TODO, but will reiterate here: recall that, if we want to denoise some
    values (SOC, Qt, R0), we will likely need to include more fields, maybe in
    new child classes defined specifically for the estimators (UKF, SPF, etc).

---
+++ asoh/models/ecm/ins_outs.py
@@ -0,0 +1,20 @@
+from pydantic import Field
+from asoh.models.base import (InputQuantities,
+                              OutputMeasurements)
+
+
+class ECMInput(InputQuantities):
+    """
+    Control of a battery based on the feed current, temperature
+    """
+    temperature: float = Field(default=None,
+                               description='Temperature reading(s). Units: °C')
+
+
+# TODO (vventuri): Remember we need to implement ways to denoise SOC, Qt, R0,
+#                   which require more outputs
+class ECMMeasurement(OutputMeasurements):
+    """
+    Controls the outputs of the ECM.
+    """
+    pass
