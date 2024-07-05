d964ee0cf4c95abc43eb4ff950a1792ea3e71d5d
victorventuri <victorventuri95@gmail.com>
2024-07-05 16:29:36 -0500

    ECM simulator can now  keep its history
    Added the possibility of keeping the full history in the simulator, including
    inputs, transient states, and measurements. We are not keeping the A-SOH history
    and for now, that does not change.

--- asoh/models/ecm/simulator.py
+++ asoh/models/ecm/simulator.py
@@ -19,18 +19,21 @@ class ECMSimulator():
             time = 0.0 seconds with a current of 0.0 Amps.
         current_behavior: Determines how to the total current behaves in-between time steps. Can be either 'constant' or
             'linear'.
+        keep_history: Boolean to determine whether we wish to keep history of the system.
     """
 
     def __init__(self,
                  asoh: ECMASOH,
                  transient_state: ECMTransientVector = None,
                  initial_input: ECMInput = None,
-                 current_behavior: Literal['constant', 'linear'] = 'constant'
+                 current_behavior: Literal['constant', 'linear'] = 'constant',
+                 keep_history: bool = False,
                  ) -> None:
         self.has_C0 = asoh.c0 is not None
         self.num_RC = len(asoh.rc_elements)
         self.current_behavior = current_behavior
         self.asoh = asoh.model_copy(deep=True)
+        self.keep_history = keep_history
         # Lenght of hidden vector: SOC + q0 + I_RC_j + hysteresis
         self.len_hidden = int(1 + int(self.has_C0) + self.num_RC + 1)
         if transient_state is None:
@@ -42,6 +45,11 @@ class ECMSimulator():
         self.measurement = ECM().calculate_terminal_voltage(new_input=self.previous_input,
                                                             transient_state=self.transient,
                                                             asoh=self.asoh)
+
+        if self.keep_history:
+            self.input_history = [self.previous_input]
+            self.transient_history = [self.transient]
+            self.measurement_history = [self.measurement]
 
     def step(self, new_input: ECMInput) -> Tuple[ECMTransientVector, ECMMeasurement]:
         """
@@ -71,6 +79,11 @@ class ECMSimulator():
 
         # Update measurement
         self.measurement = new_measurement.model_copy(deep=True)
+
+        if self.keep_history:
+            self.input_history += [self.previous_input]
+            self.transient_history += [self.transient]
+            self.measurement_history += [self.measurement]
 
         return new_transient, new_measurement
 
