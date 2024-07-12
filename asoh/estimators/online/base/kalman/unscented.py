399c63b6b45c48073f53458178e0a2a15e31f054
victorventuri <victorventuri95@gmail.com>
2024-07-12 13:32:04 -0500

    Wrote step 1 of UKF
    The full step 1 (estimation) of the UKF has been implemented. We need to write
    step 2 (correction), as well as all the necessary tests.

--- asoh/estimators/online/base/kalman/unscented.py
+++ asoh/estimators/online/base/kalman/unscented.py
@@ -1,9 +1,11 @@
 """ Defines functionality for Unscented Kálmán Filter (UKF) """
-from typing import Union, Literal, Optional, Tuple
+from typing import Union, Literal, Optional, Tuple, Dict
 
 import numpy as np
+from scipy.linalg import block_diag
 
 from asoh.estimators.online import ModelFilterInterface, OnlineEstimator, ControlVariables, OutputMeasurements
+from asoh.estimators.online.base.utils import ensure_positive_semi_definite
 from asoh.estimators.online.base.kalman import KalmanHiddenState, KalmanOutputMeasurement
 
 
@@ -14,6 +16,7 @@ class UnscentedKalmanFilter(OnlineEstimator):
     Args:
         model: model describing the system
         initial_state: initial hidden state of the system
+        initial_control: initial control on the system
         alpha_param: tuning parameter 0.001 <= alpha <= 1 used to control the
                     spread of the sigma points; lower values keep sigma
                     points closer to the mean, alpha=1 effectively brings
@@ -30,6 +33,7 @@ class UnscentedKalmanFilter(OnlineEstimator):
     def __init__(self,
                  model: ModelFilterInterface,
                  initial_state: KalmanHiddenState,
+                 initial_control: ControlVariables,
                  alpha_param: float = 1.,
                  kappa_param: Union[float, Literal['automatic']] = 0.,
                  beta_param: float = 2.,
@@ -37,6 +41,7 @@ class UnscentedKalmanFilter(OnlineEstimator):
                  covariance_sensor_noise: Optional[np.ndarray] = None):
         self.model = model
         self.state = initial_state.model_copy(deep=True)
+        self.u = initial_control.model_copy(deep=True)
         assert model.num_hidden_dimensions == initial_state.num_dimensions, \
             'Model expects %d hidden dimensions, but initial state has %d!' % \
             (model.num_hidden_dimensions, initial_state.num_dimensions)
@@ -103,24 +108,109 @@ class UnscentedKalmanFilter(OnlineEstimator):
         """
         # Step 0: build Sigma points
         sigma_pts = self.build_sigma_points()
-        # Step 1a: perform estimation step (evolution of the Sigma points)
-        evolved_sigma_pts, x_k_minus = self.estimation_update(sigma_pts=sigma_pts, u=u)
-        # Step 1b: predict output measurements from x_hat_k_minus
-        y_hat, cov_xy = self.predict_outputs(evolved_sigma_pts)
+        # Step 1: perform estimation update
+        x_k_minus, y_k, cov_xy = self.estimation_update(sigma_pts=sigma_pts, u=u)
         # Step 2: correction step, adjust hidden states based on new measurement
-        self.correction_update(y_hat=y_hat, y=y, cov_xy=cov_xy)
+        self.correction_update(x_k_minus=x_k_minus, y_hat=y_k, cov_xy=cov_xy, y=y)
         # Return
-        return (y_hat.model_copy(deep=True), self.state.model_copy(deep=True))
+        return (y_k.model_copy(deep=True), self.state.model_copy(deep=True))
 
     def build_sigma_points(self) -> np.ndarray:
         """
-        Function to build Sigma points
+        Function to build Sigma points.
+
+        Returns:
+            2D numpy array, where each row represents an "augmented state" consisting of hidden state, process noise,
+            and sensor noise, in that order.
         """
-        pass
+        # Building augmented state (recall noise terms are all zero-mean!)
+        x_aug = np.hstack((self.state.mean,
+                           np.zeros(self.model.num_hidden_dimensions + self.model.num_output_dimensions)))
+
+        # Now, build the augmented covariance
+        cov_aug = block_diag(self.state.covariance, self.cov_w, self.cov_v)
+        # Making sure this is positive semi-definite
+        cov_aug = ensure_positive_semi_definite(cov_aug)
+
+        # Sigma points are the augmented "mean" + each individual row of the transpose of the Cholesky decomposition of
+        # Cov_aug, with a weighing factor of plus and minus gamma_param
+        sqrt_cov_aug = np.linalg.cholesky(cov_aug).T
+        aux_sigma_pts = np.vstack((np.zeros((self._aug_len,)),
+                                   self.gamma_param * sqrt_cov_aug,
+                                   -self.gamma_param * sqrt_cov_aug))
+        sigma_pts = x_aug + aux_sigma_pts
+        return sigma_pts
+
+    def _break_sigma_pts(self, sigma_pts: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Function to break Sigma points into its hidden state, process noise, and sensor noise parts
+
+        Args:
+            sigma_pts: Sigma points matrix to be broken up
+
+        Returns:
+            - x_hid: array corresponding to iterable of hidden states
+            - w_hid: array corresponding to iterable of process noises
+            - v_hid: array corresponding to iterable of sensor noises
+        """
+        dim = self.model.num_hidden_dimensions
+        x_hid = sigma_pts[:, :dim].copy()
+        w_hid = sigma_pts[:, dim:(2 * dim)].copy()
+        v_hid = sigma_pts[:, (2 * dim):].copy()
+        return x_hid, w_hid, v_hid
+
+    def _evolve_hidden(self, hidden_states: np.ndarray, new_control: ControlVariables) -> np.ndarray:
+        """
+        Function used to evolve the hidden states obtained from the Sigma points
+
+        Args:
+            hidden_states: array of hidden states from breaking of the Sigma points
+            new_control: new control variables to be given to the model
+
+        Returns:
+            - x_update: updated hidden states
+        """
+        # Get old control
+        u_old = self.u.model_copy(deep=True)
+
+        # Update hidden states
+        x_update = self.model.update_hidden_states(hidden_states=hidden_states,
+                                                   previous_controls=u_old,
+                                                   new_controls=new_control)
+        return x_update
+
+    def _assemble_unscented_estimate(self, samples: np.ndarray) -> Dict:
+        """
+        Function that takes a collection of samples (either updated hidden states (evolved from the Sigma points) and
+        assembles them into a new hidden state corresponding to x_k_minus (includes mean and covariance)
+
+        Args:
+            updated_hidden_states: array of evolved hidden states
+
+        Returns:
+            x_k_minus: new estimate of the hidden state (includes mean and covariance!)
+        """
+        # Start with mean
+        mu = np.average(samples, axis=0, weights=self.mean_weights)
+
+        # For the covariance, since there can be negative weights, we need to calculate things by hand
+        diffs = samples - mu
+        cov = self._get_unscented_covariance(array0=diffs)
+        return {'mean': mu, 'covariance': cov}
+
+    def _get_unscented_covariance(self, array0: np.ndarray, array1: np.ndarray = None) -> np.ndarray:
+        """
+        Functions that computes the unscented covariance between zero-mean arrays. If second array is not provided, 
+        this is equivalent to computing the unscented variance of the only provided array. 
+        """
+        if array1 is None:
+            array1 = array0
+        cov = np.matmul(array0.T, np.matmul(np.diag(self.cov_weights), array1))
+        return cov
 
     def estimation_update(self,
                           sigma_pts: np.ndarray,
-                          u: ControlVariables) -> Tuple[np.ndarray, KalmanHiddenState]:
+                          u: ControlVariables) -> Tuple[KalmanHiddenState, KalmanOutputMeasurement, np.ndarray]:
         """
         Function to perform the estimation update from the Sigma points
 
@@ -129,26 +219,52 @@ class UnscentedKalmanFilter(OnlineEstimator):
             u: new controls to be used to evolve Sigma points
 
         Returns:
-            Evolved Sigma points, as well as new estimate of the hidden state corresponding to x_k_minus
+            - x_k_minus: new estimate of the hidden state corresponding to x_k_minus (includes mean and covariance!)
+            - y_k: estimate of the output measurement (includes mean and covariance!)
+            - cov_xy: covariance matrix between hidden state and output
         """
-        pass
+        # Step 1a: break up Sigma points to get hidden states, process errors, and sensor errors
+        x_hid, w_hid, v_hid = self._break_sigma_pts(sigma_pts=sigma_pts)
 
-    def predict_outputs(self,
-                        evolved_sigma_pts: np.ndarray) -> Tuple[KalmanOutputMeasurement, np.ndarray]:
+        # Step 1b: evolve hidden states based on the model and the previous input
+        x_updated = self._evolve_hidden(hidden_states=x_hid, new_control=u)
+        # Don't forget to include process noise!
+        x_updated += w_hid
+        # Assemble x_k_minus
+        x_k_minus_info = self._assemble_unscented_estimate(samples=x_updated)
+        x_k_minus = KalmanHiddenState.model_validate(x_k_minus_info)
+
+        # Step 1c: use updated hidden states to predict outpus
+        y_preds = self._predict_outputs(updated_hidden_states=x_updated, control=u)
+        # Don't forget to include sensor noise!
+        y_preds += v_hid
+        # Assemble y_hat
+        y_k_info = self._assemble_unscented_estimate(samples=y_preds)
+        y_k = KalmanOutputMeasurement.model_validate(y_k_info)
+        # Calculate covariance between hidden state and output
+        cov_xy = self._get_unscented_covariance(array0=(x_updated - x_k_minus.mean), array1=(y_preds - y_k.mean))
+
+        return x_k_minus, y_k, cov_xy
+
+    def _predict_outputs(self,
+                         updated_hidden_states: np.ndarray,
+                         control: ControlVariables) -> np.ndarray:
         """
         Function to predict outputs from evolved Sigma points.
 
         Args:
-            evolved_sigma_pts: numpy array of the updated Sigma points
+            updated_hidden_states: numpy array of the updated hidden states (includes process noise already)
+            control: control to be used for predicting outpus
 
         Returns:
-            Tuple composed of y_hat, the predicted output (containing mean and covariance), as well as cov_xy, the
-            updated covariance matrix between hidden states and outputs
+            y_preds: predicted outputs based on provided hidden states and control
         """
-        pass
+        y_preds = self.model.predict_measurement(hidden_states=updated_hidden_states, controls=control)
+        return y_preds
 
     def correction_update(self,
+                          x_k_minus: KalmanHiddenState,
                           y_hat: KalmanOutputMeasurement,
-                          y: OutputMeasurements,
-                          cov_xy: np.ndarray) -> None:
+                          cov_xy: np.ndarray,
+                          y: OutputMeasurements) -> None:
         pass
