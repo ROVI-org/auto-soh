ef63a86fd5998d6370bb63bff198b6c36bc8a864
victorventuri <victorventuri95@gmail.com>
2024-07-26 11:23:17 -0500

    Fixing optional fields for ECMInput
    Similar to what we did with transient, Optional fields now have to be appropriate
    BatchedVariable(batched_value=None)

--- moirae/models/ecm/ins_outs.py
+++ moirae/models/ecm/ins_outs.py
@@ -1,12 +1,14 @@
 from pydantic import Field
+
 from moirae.models.base import InputQuantities, OutputQuantities
+from moirae.models.utils import convert_single_valued
 
 
 class ECMInput(InputQuantities):
     """
     Control of a battery based on the feed current, temperature
     """
-    temperature: float = Field(default=None,
+    temperature: float = Field(default_factory=lambda: convert_single_valued(value=None),
                                description='Temperature reading(s). Units: °C')
 
 
