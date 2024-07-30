95455c3406c9321a219b7f86f3df7492fe4750ac
lward <lward@anl.gov>
2024-07-30 14:55:56 -0400

    Implement and test batdata interface

--- moirae/interface.py
+++ moirae/interface.py
@@ -2,6 +2,7 @@
 from typing import Tuple
 from math import isfinite
 
+import numpy as np
 import pandas as pd
 from batdata.data import BatteryDataset
 
@@ -41,7 +42,7 @@ def _row_to_inputs(row: pd.Series, default_temperature: float = 25) -> Tuple[Del
 def run_online_estimate(
         dataset: BatteryDataset,
         estimator: OnlineEstimator
-) -> pd.DataFrame:
+) -> Tuple[np.ndarray, OnlineEstimator]:
     """Run an online estimation of battery parameters given a fixed dataset for the
 
     Args:
@@ -50,7 +51,8 @@ def run_online_estimate(
             a physics model which describes the cell and initial guesses for the battery
             transient and health states.
     Returns:
-        Estimates of the parameters at all timesteps from the input dataset
+        - Estimates of the parameters at all timesteps from the input dataset
+        - Estimator after updating with the data in dataset
     """
 
     # Ensure raw data are present in the data file
@@ -61,5 +63,20 @@ def run_online_estimate(
     initial_input, _ = _row_to_inputs(dataset.raw_data.iloc[0])
     estimator.u = initial_input
 
-    # Iterate over all
-    raise NotImplementedError()
+    # Initialize the output arrays
+    state_mean = np.zeros((len(dataset.raw_data), estimator.num_hidden_dimensions))
+    state_std = np.zeros((len(dataset.raw_data), estimator.num_hidden_dimensions))
+    output_mean = np.zeros((len(dataset.raw_data), estimator.num_output_dimensions))
+    output_std = np.zeros((len(dataset.raw_data), estimator.num_output_dimensions))
+
+    # Iterate over all timesteps
+    for i, (_, row) in enumerate(dataset.raw_data.reset_index().iterrows()):  # .reset_index to iterate in sort order
+        controls, measurements = _row_to_inputs(row)
+        new_outputs, new_state = estimator.step(controls, measurements)
+
+        state_mean[i, :] = new_state.get_mean()
+        state_std[i, :] = np.diag(new_state.get_covariance())
+        output_mean[i, :] = new_outputs.get_mean()
+        output_std[i, :] = np.diag(new_outputs.get_covariance())
+
+    return state_mean, estimator
