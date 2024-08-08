d52d5c3f14e2fdd654900b49bd7cb631d9351206
lward <lward@anl.gov>
2024-08-08 12:14:49 -0400

    Add version module

---
+++ moirae/version.py
@@ -0,0 +1,5 @@
+import importlib.metadata
+
+# single source of truth for package version,
+# see https://packaging.python.org/en/latest/single_source_version/
+__version__ = importlib.metadata.version('moirae')
