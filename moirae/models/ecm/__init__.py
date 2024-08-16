Fix ECM broadcasting issues (#66)
* Fixing broadcasting issues

In numpy, the '+=' operation does not follow broadcasting rules, as it performs
the operation in place, rather than creting a copy. Therefore, we had to
substitute all of them.

* Fixing broadcasting issues

For interpolated variables, a number of issues popped up when the batch sizes
were not the same. Some of them are fixed, hopefully all, but not sure. Also, as
in the previous commit, '+=' is not broadcastable, so I had to fix that here too.

* Adding an appropriate batch test

Here, I add a test that mix-and-matches batched and un-batched transient vectors
and A-SOH, to make sure all operations run as intended.

* Making sure second dimension is correct in batched tests

* Optimizing numpy operations

There were a number of copies of numpy arrays that were not necessary. We have
changed it so that we only copy things which are changing to store them in a
different variable, but if it is part of an operation, the copy has been removed.

moirae/models/ecm/__init__.py
@@ -97,7 +97,7 @@ class EquivalentCircuitModel(CellModel):
         # We need to figure out if the current changes sign during this process
 
         if current_k * current_kp1 >= 0 or self.current_behavior == 'constant':
-            hyst_kp1 = hysteresis_solver_const_sign(h0=transient_state.hyst.copy(),
+            hyst_kp1 = hysteresis_solver_const_sign(h0=transient_state.hyst,
                                                     M=M,
                                                     kappa=kappa,
                                                     dt=delta_t,
@@ -107,7 +107,7 @@ class EquivalentCircuitModel(CellModel):
         else:
             # solving for time until current == 0
             phi = -current_k / current_slope
-            h_mid = hysteresis_solver_const_sign(h0=transient_state.hyst.copy(),
+            h_mid = hysteresis_solver_const_sign(h0=transient_state.hyst,
                                                  M=M,
                                                  kappa=kappa,
                                                  dt=phi,
@@ -145,12 +145,13 @@ class EquivalentCircuitModel(CellModel):
         Vt = asoh.ocv(soc=transient_state.soc, temp=new_inputs.temperature)
 
         # Add I*R drop ('DCIR')
-        Vt += new_inputs.current * asoh.r0.get_value(soc=transient_state.soc,
-                                                     temp=new_inputs.temperature)
+        IR_drop = new_inputs.current * asoh.r0.get_value(soc=transient_state.soc,
+                                                         temp=new_inputs.temperature)
+        Vt = Vt + IR_drop
 
         # Check series capacitance
         if transient_state.q0 is not None:
-            Vt += transient_state.q0.copy() / asoh.c0.get_value(soc=transient_state.soc.copy())
+            Vt = Vt + transient_state.q0 / asoh.c0.get_value(soc=transient_state.soc.copy())
 
         # Check RC elements
         if transient_state.i_rc.shape[-1] > 0:
@@ -160,9 +161,9 @@ class EquivalentCircuitModel(CellModel):
                  for rc in asoh.rc_elements]
             )  # Shape: (rc_rs, batch_dim, 1 resistance)
             V_drops = transient_state.i_rc * rc_rs[:, :, 0].T
-            Vt += np.sum(V_drops, axis=1, keepdims=True)
+            Vt = Vt + np.sum(V_drops, axis=1, keepdims=True)
 
         # Include hysteresis
-        Vt += transient_state.hyst
+        Vt = Vt + transient_state.hyst
 
         return ECMMeasurement(terminal_voltage=Vt)

moirae/models/ecm/utils.py
@@ -61,6 +61,8 @@ class SOCInterpolatedHealth(HealthVariable):
             y = self.base_values[:, 0]
             if soc_batch_size > 0 and batch_size == 1:
                 return np.repeat(y, soc.size, axis=0).reshape(input_dims)
+            elif soc_batch_size == 1 and batch_size > 1:
+                return y.reshape((batch_size,) + input_dims)
             return y.reshape(input_dims)
 
         # Run the interpolator, but the results mean something different
@@ -132,5 +134,5 @@ def hysteresis_solver_const_sign(
         exp_factor = -exp_factor
     exp_factor = np.exp(exp_factor)
     h_dt = exp_factor * h0
-    h_dt += (1 - exp_factor) * M
+    h_dt = h_dt + ((1 - exp_factor) * M)
     return h_dt