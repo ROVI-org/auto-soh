9a672bf50dadc31b344d1867c7446921aec458ce
victorventuri <victorventuri95@gmail.com>
2024-09-10 16:50:23 -0500

    Adding degradation model to simulator

--- moirae/simulator.py
+++ moirae/simulator.py
@@ -5,7 +5,13 @@ import pandas as pd
 from typing import Tuple, List, Optional
 
 from batdata.data import BatteryDataset
-from moirae.models.base import HealthVariable, GeneralContainer, InputQuantities, CellModel, OutputQuantities
+from moirae.models.base import (HealthVariable,
+                                GeneralContainer,
+                                InputQuantities,
+                                OutputQuantities,
+                                CellModel,
+                                DegradationModel)
+from moirae.models.utils import DummyDegradation
 
 
 class Simulator:
@@ -19,10 +25,11 @@ class Simulator:
     if ``keep_history`` is True.
 
     Args:
-        model: Model used to simulate the battery system
+        cell_model: Model used to simulate the battery system
         asoh: Advanced State of Health (A-SOH) of the system. Used to parametrize the dynamics of the system.
         transient_state: Initial transient state of the system
         initial_input: Initial input of the ECM
+        degradation_model: Degradation model to be used for degrading the A-SOH; defaults to no degradation
         keep_history: Whether to keep history of the system.
     """
 
@@ -39,16 +46,16 @@ class Simulator:
     """Health variables for each of the cells being simulated"""
     previous_input: InputQuantities
     """Last inputs to the system"""
-    measurement: OutputQuantities
-    """Last measurement result"""
 
     def __init__(self,
-                 model: CellModel,
+                 cell_model: CellModel,
                  asoh: HealthVariable,
                  transient_state: GeneralContainer,
                  initial_input: InputQuantities,
+                 degradation_model: DegradationModel = DummyDegradation(),
                  keep_history: bool = False):
-        self.model = model
+        self.model = cell_model
+        self.degradation_model = degradation_model
 
         # Store copies of the initial states
         self.asoh = asoh.model_copy(deep=True)
@@ -65,6 +72,7 @@ class Simulator:
         if self.keep_history:
             self.input_history = [self.previous_input.model_copy(deep=True)]
             self.transient_history = [self.transient.model_copy(deep=True)]
+            self.asoh_history = [self.asoh.model_copy(deep=True)]
             self.measurement_history = [self.measurement.model_copy(deep=True)]
         else:
             self.input_history = self.transient_history = self.measurement_history = None
@@ -97,9 +105,17 @@ class Simulator:
         # Update measurement
         self.measurement = new_measurement.model_copy(deep=True)
 
+        # Degrade A-SOH
+        new_asoh = self.degradation_model.update_asoh(previous_asoh=self.asoh,
+                                                      new_inputs=new_inputs,
+                                                      new_transients=new_transient,
+                                                      new_measurements=new_measurement)
+        self.asoh = new_asoh.model_copy(deep=True)
+
         if self.keep_history:
             self.input_history.append(self.previous_input)
             self.transient_history.append(new_transient)
+            self.asoh_history.append(new_asoh)
             self.measurement_history.append(new_measurement)
 
         return new_transient, new_measurement
