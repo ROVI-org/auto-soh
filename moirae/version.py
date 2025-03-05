d537aee08506a5079a515073e96d58283c4a94e7
Logan Ward <ward.logan.t@gmail.com>
2025-03-05 08:35:07 -0500

    Add function to return git version

--- moirae/version.py
+++ moirae/version.py
@@ -1,5 +1,25 @@
+from functools import cache
+from subprocess import run
+from shutil import which
 import importlib.metadata
 
 # single source of truth for package version,
 # see https://packaging.python.org/en/latest/single_source_version/
 __version__ = importlib.metadata.version('moirae')
+
+
+@cache
+def load_git_version() -> str:
+    """Load the git version of the git repo
+
+    Returns:
+        Git commit summary, if available. ``None`` otherwise.
+    """
+
+    if which("git") is None:
+        raise ValueError('Cannot find git')
+    proc = run('git describe --long --tags --dirty --always'.split(),
+               capture_output=True, text=True)
+    if proc.returncode != 0:
+        raise ValueError(f'Git failed: {proc.stderr}')
+    return proc.stdout.strip()
---
+++ tests/test_version.py
@@ -0,0 +1,7 @@
+from moirae.version import load_git_version
+
+
+def test_git():
+    version = load_git_version()
+    assert isinstance(version, str)
+    assert len(version) >= 8
