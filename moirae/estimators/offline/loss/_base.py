24901086dfd980c5775bfe15fad816bfe5f17a63
Logan Ward <ward.logan.t@gmail.com>
2025-03-25 10:10:07 -0400

    Introduce a prior loss function

---
+++ moirae/estimators/offline/loss/_base.py
@@ -0,0 +1,83 @@
+from functools import cached_property
+
+import numpy as np
+
+from battdat.data import BatteryDataset
+from moirae.models.base import CellModel, HealthVariable, GeneralContainer
+
+
+# TODO (wardlt): Add degradation model after we decide how to handle its parameters
+class BaseLoss:
+    """
+    Base class for objective functions which evaluate the ability of a set
+    of battery health parameters to explain the observed performance data.
+
+    All Loss classes should follow the convention that better sets of
+    parameters yield values which are less than worse parameters.
+    There are no constraints on whether the values need to be positive or negative.
+    """
+
+    cell_model: CellModel
+    """Cell model used to compute """
+    asoh: HealthVariable
+    """Initial guess for battery health"""
+    state: GeneralContainer
+    """Initial guess for battery transient state"""
+    observations: BatteryDataset
+    """Observed data for the battery performance"""
+
+    def __init__(self,
+                 cell_model: CellModel,
+                 asoh: HealthVariable,
+                 transient_state: GeneralContainer,
+                 observations: BatteryDataset):
+        self.cell_model = cell_model
+        self.asoh = asoh.model_copy(deep=True)
+        self.state = transient_state.model_copy(deep=True)
+        self.observations = observations
+
+    @cached_property
+    def num_states(self) -> int:
+        """Number of output variables which correspond to transient states"""
+        return len(self.state)
+
+    def x_to_state(self, x: np.ndarray, inplace: bool = True) -> tuple[GeneralContainer, HealthVariable]:
+        """Copy batch of parameters into ASOH and state classes
+
+        Args:
+            x: Batch of parameters
+            inplace: Whether to edit the copies of ASOH and state held by the loss function,
+                or return a copy
+        """
+        states = x[:, :self.num_states]
+        asoh = x[:, self.num_states:]
+
+        if inplace:
+            self.state.from_numpy(states)
+            self.asoh.update_parameters(asoh)
+            return self.state, self.asoh
+        else:
+            return self.state.make_copy(states), self.asoh.make_copy(asoh)
+
+    # TODO (wardlt): Consider passing the ASOH parameters through somewhere else
+    def get_x0(self) -> np.ndarray:
+        """Generate an initial guess
+
+        Returns:
+            A 1D vector used as a starting point for class to this class
+        """
+        if self.asoh.num_updatable == 0:
+            return self.state.to_numpy()[0, :]
+        return np.concatenate([self.state.to_numpy(), self.asoh.get_parameters()], axis=1)[0, :]  # Get a 1D vector
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        """
+        Compute the fitness of a set of parameters to the observed data.
+
+        Args:
+            x: Batch of parameters to be evaluated (2D array)
+
+        Returns:
+            A single measure of parameter fitness for each member of batch (1D array)
+        """
+        raise NotImplementedError()
--- tests/estimators/offline/test_objectives.py
+++ tests/estimators/offline/test_objectives.py
@@ -1,7 +1,8 @@
+from scipy.stats import norm
 import numpy as np
 
 from pytest import mark
-from moirae.estimators.offline.loss import MeanSquaredLoss
+from moirae.estimators.offline.loss import MeanSquaredLoss, PriorLoss
 
 
 @mark.parametrize('state_only', [True, False])
@@ -28,3 +29,38 @@ def test_mse_loss(simple_rint, timeseries_dataset, state_only):
     y = loss(x0)
     assert y.shape == (1,)
     assert np.isclose(y, 0.)
+
+
+@mark.parametrize('state_only', [True, False])
+def test_prior_loss(simple_rint, timeseries_dataset, state_only):
+    rint_asoh, rint_state, _, ecm_model = simple_rint
+    if not state_only:
+        rint_asoh.mark_updatable('q_t.base_values')
+        assert rint_asoh.num_updatable == 1
+
+    # Assume the q_t with a normal distribution around 10 with a std of 1,
+    #  and the hyst with a mean of 0 and std of 0.1
+    qt_dist = norm(loc=10, scale=1)
+    hy_dist = norm(loc=0, scale=0.1)
+
+    loss = PriorLoss(
+        transient_priors={'hyst': hy_dist},
+        asoh_priors={} if state_only else {'q_t.base_values': qt_dist},
+        cell_model=ecm_model,
+        asoh=rint_asoh,
+        transient_state=rint_state,
+        observations=timeseries_dataset
+    )
+
+    # Get a starting guess
+    if state_only:
+        x0 = rint_state.to_numpy()
+    else:
+        x0 = np.concatenate([rint_state.to_numpy(), rint_asoh.q_t.base_values], axis=1)
+
+    # Run the evaluation
+    y = loss(x0)
+    assert y.shape == (1,)
+    qt_logprob = qt_dist.logpdf(10.)
+    hy_logprob = hy_dist.logpdf(0.)
+    assert np.isclose(y, -hy_logprob if state_only else -(qt_logprob + hy_logprob))
