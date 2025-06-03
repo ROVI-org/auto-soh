b5a4dcf2dd2d8c42d12e7e6335f952a04246a4db
victorventuri <victorventuri95@gmail.com>
2025-06-03 15:11:50 -0500

    Computing R and C with better current estimate
    Now that we have a more reliable estimate of the current that flows through the
    resistive element of the RC component at the beginning of the rest period, we
    can more accurately compute the values of R and C from the fitted A and tau
    parameters.

--- moirae/extractors/ecm/rc_components.py
+++ moirae/extractors/ecm/rc_components.py
@@ -155,8 +155,9 @@ class RCExtractor(BaseExtractor):
                                            timestamps=rest['Iprev']['time'],
                                            tau_values=tau).item()
                     # Update parameters
-                    params_rc[2 * i_rc] /= abs(i_r_rc)  # R = A/Iprev
-                    params_rc[2 * i_rc + 1] /= params_fit[2 * i_rc]  # C = T/R
+                    resitance = params_rc[2 * i_rc] / abs(i_r_rc)  # R = A/Iprev
+                    params_rc[2 * i_rc] = resitance
+                    params_rc[2 * i_rc + 1] = tau / resitance  # C = T/R
 
                     RCs[f'R{i_rc + 1}'].append(params_rc[2 * i_rc])
                     RCs[f'C{i_rc + 1}'].append(params_rc[2 * i_rc + 1])
