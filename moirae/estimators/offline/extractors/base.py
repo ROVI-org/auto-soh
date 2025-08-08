39e66d41f9868e6b1abf1410bae13ca2a92877a2
victorventuri <victorventuri95@gmail.com>
2025-08-08 15:22:18 -0500

    First pass on a capacity check data checker
    Here, we implement a capacity check data checker, which checks if the data
    provided has segments that reach the voltage limits, and which are slow enough
    to be considered capacity checks.

--- moirae/estimators/offline/extractors/base.py
+++ moirae/estimators/offline/extractors/base.py
@@ -35,37 +35,15 @@ class BaseDataChecker():
 class BaseExtractor:
     """Base class for tools which determine parameters from special cycles"""
 
-    def check_data(self, data: BatteryDataset):
-        """Verify whether data contains features needed for algorithm
-
-        Args:
-            data: Data to be evaluated
-        Raises:
-            (ValueError) If the dataset is missing critical information
-        """
-        pass  # Default: no constraints
-
-    def extract(self, data: BatteryDataset) -> HealthVariable:
-        """Determine parameters of a physics model from battery dataset
+    def extract(self, data: Union[pd.DataFrame, BatteryDataset]) -> HealthVariable:
+        """Determine parameters of a physics model from battery dataset or from raw data, which must follow the format
+        defined in `RawData format`_ of battery-data-toolkit RawData, given at
+        https://rovi-org.github.io/battery-data-toolkit/user-guide/schemas/column-schema.html#rawdata
 
         Args:
             data: Data to use for parameter assessment
+
         Returns:
             Part of the parameter set for a model.
         """
         raise NotImplementedError()
-
-    def extract_from_raw(self, data: pd.DataFrame) -> HealthVariable:
-        """Determine parameters of a physics model from current/voltage data over time
-
-        The data must follow the `RawData format`_ of battery-data-toolkit RawData.
-
-        Args:
-            data: Data to use for parameter assessment
-        Returns:
-            Part of the parameter set for a model.
-
-        .. _RawData format:
-            https://rovi-org.github.io/battery-data-toolkit/user-guide/schemas/column-schema.html#rawdata
-        """
-        return self.extract(BatteryDataset.make_cell_dataset(raw_data=data))
