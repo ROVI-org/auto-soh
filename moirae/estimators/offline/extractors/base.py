f4e7fbeb0a0b4dca26f1916fde06cce3fa8115c0
victorventuri <victorventuri95@gmail.com>
2025-08-19 13:35:53 -0500

    adding the ability to extract values without checks in base

--- moirae/estimators/offline/extractors/base.py
+++ moirae/estimators/offline/extractors/base.py
@@ -25,17 +25,37 @@ class BaseExtractor:
     """
     Base class for tools which determine parameters from special cycles
     """
+    def compute_parameters(self, data: Union[pd.DataFrame, BatteryDataset], *args, **kwargs) -> ExtractedParameter:
+        """
+        Function to compute parameters, assuming the data being provided has already been appropriately vetted for
+        extraction.
 
-    def extract(self, data: Union[pd.DataFrame, BatteryDataset]) -> ExtractedParameter:
-        """Determine parameters of a physics model from battery dataset or from raw data, which must follow the format
+        Args:
+            data: Data to use for parameter assessment as either raw dataframe, or BatterDataset object
+            *args: necessary ordered arguments for the calculation of parameters
+            **kwargs: keyword arguments required for the calculation of parameters
+
+        Returns:
+            extracted parameter(s)
+        """
+        raise NotImplementedError('Please implement in child classes!')
+
+    def extract(self, data: Union[pd.DataFrame, BatteryDataset], *args, **kwargs) -> ExtractedParameter:
+        """
+        Determine parameters of a physics model from battery dataset or from raw data, which must follow the format
         defined in `RawData format`_ of battery-data-toolkit RawData, given at
         https://rovi-org.github.io/battery-data-toolkit/user-guide/schemas/column-schema.html#rawdata
 
+        The data used will be checked and processed first. If data checks are deemed unnecessary, please use the method
+        `compute_parameters` instead.
+
         Args:
             data: Data to use for parameter assessment
+            *args: necessary ordered arguments for the extraction
+            **kwargs: keyword arguments required for the extraction
 
         Returns:
             Dictionary containing extracted values, units, and, when appropriate, additional information (such as the
             corresponding SOC levels for reported values)
         """
-        raise NotImplementedError()
+        raise NotImplementedError('Please implement in child classes!')
