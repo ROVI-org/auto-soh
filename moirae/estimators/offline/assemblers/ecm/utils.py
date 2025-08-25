8fceeadb09cf58c46d8a7dc0e0abaa97879a130b
victorventuri <victorventuri95@gmail.com>
2025-08-25 14:19:21 -0500

    creating general SOC-dependent assembler

--- moirae/estimators/offline/assemblers/ecm/utils.py
+++ moirae/estimators/offline/assemblers/ecm/utils.py
@@ -1,114 +1,111 @@
 """
-Utility functions for assemblers
+Utility functions for assembling ECM-related Health Variables from Extracted Parameters
 """
-from typing import Callable, Dict, List, Literal
+from typing import Dict, Union
 
 import numpy as np
-from scipy.interpolate import make_interp_spline, make_smoothing_spline, make_lsq_spline
-from sklearn.isotonic import IsotonicRegression
+import pandas as pd
+
+from moirae.models.ecm.components import SOCInterpolatedHealth
+from moirae.estimators.offline.extractors.base import ExtractedParameter
+from moirae.estimators.offline.assemblers.base import BaseAssembler
+from moirae.estimators.offline.assemblers.utils import SOCRegressor
 
 
-class SOCFitters():
+def post_process_extracted(extracted_parameter: ExtractedParameter) -> ExtractedParameter:
     """
-    Helper to deal with cases where variable is SOC dependent and we wish to fit a functional form to the extracted data
+    Post-processes extracted parameters, ensuring they are sorted and contain no repeated SOC entries.
+
+    Duplicated SOC values are averaged
 
     Args:
-        style: establishes the style of interpolation; options include:
-            - 'interpolate': linearly interpolates between the points provided, which are fully captured in the final
-                function
-            - 'smooth': fits a smoothing spline through the data points, where regularization penalizes the
-                integral of the squared second derivative function.
-            - 'lsq': fits a Least-Squares spline to minimize the least squares errors; requires knots to be specified
-            - 'isotonic': fits an isotonic curve, that is, a non-linear monotonic trend
-        parameters: Additional parameters needed to create fits; should not be required at fitting time
+        extracted_parameter: properly formatted dictionary with the Extracted parameter
+
+    Returns:
+        a cleaned-up version of the extracted parameters
+    """
+    # To make things easy, we will cast everything as a pandas dataframe
+    df = pd.DataFrame.from_dict(extracted_parameter)
+
+    # We don't need to keep the units there
+    df.drop('units', axis=1, inplace=True)
+
+    # Now, we will sort by SOC
+    df.sort_values(by='soc_level', inplace=True)
+
+    # Average the repeated values
+    df = df.groupby('soc_level').mean().reset_index()
+
+    # Return to dictionary format
+    info = df.to_dict(orient='list')  # 'list' ensures we skip the index of the DataFrame
+    info['units'] = extracted_parameter['units']
+    return info
+
+
+class SOCDependentAssembler(BaseAssembler):
+    """
+    Assembles an Assember object from extracted parameters.
+
+    Args:
+        regressor: object to perform SOC regression
+        soc_points: points in the SOC domain to be used when assembling the HealthVariable object
     """
     def __init__(self,
-                 style: Literal['interpolate', 'smooth', 'lsq'] = 'interpolate',
-                 parameters: Dict = {}):
-        self.style = style
-        self.parameters = parameters
+                 regressor: SOCRegressor,
+                 soc_points: Union[np.ndarray, int] = 11):
+        self.regressor = regressor
+        self.soc_points = soc_points
 
     @property
-    def accepted_styles(self) -> List[str]:
+    def soc_points(self) -> np.ndarray:
+        return self._soc_pts
+
+    @soc_points.setter
+    def soc_pointsn(self, array: Union[np.ndarray, int]):
+        if isinstance(array, int):
+            soc_pts = np.linspace(0., 1., array)
+        else:
+            soc_pts = array.copy()
+        min_val = min(soc_pts)
+        max_val = max(soc_pts)
+        if (min_val > 0.) or (max_val < 1.):
+            raise ValueError(f'SOC domain not fully covered, only {100*min_val:.1f} -- {100*max_val:.1f}%!')
+        self._soc_pts = soc_pts
+
+    def _prepare_for_regression(self, extracted_parameter: ExtractedParameter) -> Dict:
         """
-        Defines acceptable interpolation styles
-        """
-        return ['interpolate', 'smooth', 'lsq', 'isotonic']
-
-    def acceptable_keywords(self) -> List[str]:
-        """
-        Specifies what keywords are acceptable given the present style chosen
-        """
-        if self._style == 'interpolate':
-            return ['k', 't', 'bc_type', 'axis', 'check_finite']
-        elif self._style == 'smooth':
-            return ['lam', 'axis']
-        elif self._style == 'lsq':
-            return ['k', 'w', 'axis', 'check_finite', 'method']
-        elif self._style == 'isotonic':
-            return ['y_min', 'y_max', 'increasing', 'out_of_bounds']
-
-    @property
-    def style(self) -> Literal['interpolate', 'smooth', 'lsq']:
-        return self._style
-
-    @style.setter
-    def style(self, value: Literal['interpolate', 'smooth', 'lsq']):
-        if value not in self.accepted_styles:
-            message = 'Accepted fitting styles are '
-            message += ', '.join(self.accepted_styles)
-            message += f', not {value}!'
-            raise ValueError(message)
-        self._style = value
-
-    @property
-    def parameters(self) -> Dict:
-        return self._params.copy()
-
-    @parameters.setter
-    def parameters(self, params: Dict):
-        if not isinstance(params, Dict):
-            raise TypeError('Parameters must be provided as a dictionary!')
-        for key in params.keys():
-            if key not in self.acceptable_keywords():
-                message = f'Acceptable parameters for {self.style} are '
-                message += ', '.join(self.acceptable_keywords()) + f', not {key}!'
-                raise ValueError(message)
-        self._params = params.copy()
-
-    def fit(self, soc: np.ndarray, targets: np.ndarray, **kwargs) -> Callable:
-        """
-        Fits a function that tries to match the SOC values provided to the target values. Additional keywords are used
-        to update the construction of the fitting.
+        Auxiliary function to prepare dictionary to be given directly to the regressor at the time of fitting
 
         Args:
-            soc: values of SOC to be used
-            targets: values the function is trying to match
-            **kwargs: additional arguments that will be used in the construction of the regressor
+            extracted_parameter: dictionary containing information about the extracted parameter
 
         Returns:
-            function that takes SOC values and outputs predictions
+            dictionary of keywords to be used 
         """
-        if self._style == 'interpolate':
-            return make_interp_spline(x=soc, y=targets, **self.parameters.update(kwargs))
-        elif self._style == 'smooth':
-            return make_smoothing_spline(x=soc, y=targets, **self.parameters.update(kwargs))
-        elif self._style == 'lsq':
-            if 't' not in self._params.keys() and 't' not in kwargs.keys():
-                raise ValueError('Please provide knots for LSQ Spline!')
-            return make_lsq_spline(x=soc, y=targets, **self.parameters.update(kwargs))
-        elif self._style == 'isotonic':
-            # If sample weights are present, they need to be treated separately
-            sample_weight = None
-            # Update all other parameters
-            params = self.parameters
-            for key, val in kwargs.items():
-                if key == 'sample_weight':
-                    sample_weight = val
-                else:
-                    params.update({key: val})
-            # Build regression
-            regressor = IsotonicRegression(**params)
-            # Fit
-            regressor.fit(X=soc, y=targets, sample_weight=sample_weight)
-            return regressor
+        # Clean-up the parameters
+        clean_param = post_process_extracted(extracted_parameter=extracted_parameter)
+
+        regression_dict = {'soc': clean_param['soc_level'],
+                           'targets': clean_param['value']}
+        
+        for key in clean_param.keys():
+            if (key != 'units') and (key != 'soc_level') and (key != 'value'):
+                regression_dict['key'] = clean_param[key]
+
+        # Make sure to add knots in the case of LSQ Univariate regression!
+        if self.regressor.style == 'lsq':
+            regression_dict['t'] = self._soc_pts
+
+        return regression_dict
+
+    def assemble(self, extracted_parameter: ExtractedParameter) -> SOCInterpolatedHealth:
+        # Get dictionary of parameters to fit
+        regression_dict = self._prepare_for_regression(extracted_parameter=extracted_parameter)
+
+        # Fit
+        fit = self.regressor.fit(**regression_dict)
+
+        # Compute values at desired points
+        vals = fit(self._soc_pts)
+
+        return SOCInterpolatedHealth(base_values=vals, soc_pinpoints=self._soc_pts)
