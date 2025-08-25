d1a3c220c5ae665ff21fb38d646e832642fbfecb
victorventuri <victorventuri95@gmail.com>
2025-08-25 14:59:12 -0500

    first test for assemblers

---
+++ tests/estimators/offline/assemblers/test_utils.py
@@ -0,0 +1,35 @@
+from pytest import raises
+
+from moirae.estimators.offline.assemblers.utils import SOCRegressor
+
+
+def test_wrong_init():
+    # Testing wrong style
+    with raises(ValueError, match="Accepted fitting styles are"):
+        test = SOCRegressor(style='splin')
+
+    # Test wrong parameter type
+    with raises(TypeError, match="Parameters must be provided as a dictionary!"):
+        test = SOCRegressor(parameters=[1, 2, 3])
+
+    # Test wrong parameters
+    # Simple Interpolation
+    mess = 'Acceptable parameters for interpolate are k, t, bc_type, axis, check_finite, '
+    mess += 'not abcd!'
+    with raises(ValueError, match=mess):
+        test = SOCRegressor(parameters={'abcd': 1234})
+    # Spline
+    mess = 'Acceptable parameters for smooth are lam, axis, '
+    mess += 'not abcd!'
+    with raises(ValueError, match=mess):
+        test = SOCRegressor(style='smooth', parameters={'abcd': 1234})
+    # LSQ
+    mess = 'Acceptable parameters for lsq are k, w, axis, check_finite, method, '
+    mess += 'not abcd!'
+    with raises(ValueError, match=mess):
+        test = SOCRegressor(style='lsq', parameters={'abcd': 1234})
+    # Isotonic
+    mess = 'Acceptable parameters for isotonic are y_min, y_max, increasing, out_of_bounds, '
+    mess += 'not abcd!'
+    with raises(ValueError, match=mess):
+        test = SOCRegressor(style='isotonic', parameters={'abcd': 1234})
